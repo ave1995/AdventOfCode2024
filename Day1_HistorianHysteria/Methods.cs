724d9e1b691ea743cfa0af51328d07ca18b00901
ave <veselya95@gmail.com>
2024-12-01 20:42:28 +0100

    Better way

--- Day1_HistorianHysteria/Methods.cs
+++ Day1_HistorianHysteria/Methods.cs
@@ -7,7 +7,7 @@ public static class Methods
     [MeasureExecutionTimeAspect]
     public static int CountTotalDistance(string input, bool log = false)
     {
-        var (column1, column2) = ParseInput(input);
+        var (column1, column2) = ParseInput(input).OrderArray();
 
         var totalCount = 0;
 
@@ -22,100 +22,58 @@ public static class Methods
         //return column2.Select((t, i) => Math.Abs(column1[i] - t)).Sum();
     }
 
+    //26674158
     [MeasureExecutionTimeAspect]
     public static int CountSimilarityScore(string input)
     {
-        var (column1, column2) = ParseInput(input);
-
-        var totalSimilarity = 0;
-        var ignoredIndex = 0;
+        var (array, length) = ParseInput(input);
+        var dictionary = new Dictionary<int, int>();
         
-        foreach (var column1Value in column1)
+        var totalSimilarity = 0;
+
+        for (var i = 0; i < length; i++)
         {
-            var firstOccurence = BinarySearchFirstOccurence(column2.Skip(ignoredIndex).ToArray(), column1Value);
-            var lastOccurence = BinarySearchLastOccurence(column2.Skip(ignoredIndex).ToArray(), column1Value);
-            
-            if (firstOccurence == -1 || lastOccurence == -1) continue;
-            
-            var occurence = lastOccurence - firstOccurence + 1;
-            totalSimilarity += occurence * column1Value;
-            ignoredIndex = lastOccurence + 1;
+            var value = array[i, 1];
+            if(!dictionary.TryAdd(value, 1))
+                dictionary[value]++;
+        }
+
+        for (var i = 0; i < length; i++)
+        {
+            var value = array[i, 0];
+            totalSimilarity += dictionary.TryGetValue(value, out var occurence) ? occurence * value : 0;
         }
 
         return totalSimilarity;
     }
     
-    private static (int[] column1, int[] column2) ParseInput(string input)
+    private static (int[,] array2X, int length) ParseInput(string input)
     {
         var rows = input.Split(['\n'], StringSplitOptions.RemoveEmptyEntries);
 
         var array = new int[rows.Length, 2];
 
-        for (int i = 0; i < rows.Length; i++)
+        for (var i = 0; i < rows.Length; i++)
         {
             var columns = rows[i].Split([' '], StringSplitOptions.RemoveEmptyEntries);
             array[i, 0] = int.Parse(columns[0]);
             array[i, 1] = int.Parse(columns[1]);
         }
+        return (array, rows.Length);
+    }
 
-        var column1 = Enumerable.Range(0, array.GetLength(0))
-            .Select(i => array[i, 0])
+    private static (int[] column1, int[] column2) OrderArray(this (int[,] array, int length) tuple)
+    {
+        var column1 = Enumerable.Range(0, tuple.length)
+            .Select(i => tuple.array[i, 0])
             .OrderBy(i => i)
             .ToArray();
 
-        var column2 = Enumerable.Range(0, array.GetLength(0))
-            .Select(i => array[i, 1])
+        var column2 = Enumerable.Range(0, tuple.length)
+            .Select(i => tuple.array[i, 1])
             .OrderBy(i => i)
             .ToArray();
 
         return (column1, column2);
-    }
-
-    private static int BinarySearchFirstOccurence(int[] array, int target)
-    {
-        int low = 0;
-        int high = array.Length - 1;
-        int result = -1;
-
-        while (low <= high)
-        {
-            int middle = low + (high - low) / 2;
-
-            if (array[middle] == target)
-            {
-                result = middle;
-                high = middle - 1; // Move left to find the first occurrence
-            }
-            else if (array[middle] < target)
-                low = middle + 1;
-            else
-                high = middle - 1;
-        }
-
-        return result;
-    }
-    
-    private static int BinarySearchLastOccurence(int[] array, int target)
-    {
-        int low = 0;
-        int high = array.Length - 1;
-        int result = -1;
-
-        while (low <= high)
-        {
-            int middle = low + (high - low) / 2;
-
-            if (array[middle] == target)
-            {
-                result = middle;
-                low = middle + 1; // Move right to find the last occurrence
-            }
-            else if (array[middle] < target)
-                low = middle + 1;
-            else
-                high = middle - 1;
-        }
-
-        return result;
     }
 }
